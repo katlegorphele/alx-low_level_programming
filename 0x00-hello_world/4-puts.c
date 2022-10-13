265873832d8477239a926ef57458ddb8340b6de1
Kat-Devs <katlegophele95@gmail.com>
2022-10-13 10:54:55 -0700

    Script 5 added, script 4 modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+/*
+   * main - main function
+   *
+   * Return: void
+*/
+
 int main(void)
 {
+	/* string initialisation*/
 	char MyStr[] = "\"Programming is like building a multilingual puzzle";
-	
 	puts(MyStr);
-
-	return 0;
+	return (0);
 }
