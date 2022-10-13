dcae566812315d5990834895d5d806d0862fae29
Kat-Devs <katlegophele95@gmail.com>
2022-10-13 14:40:46 -0700

    update for script 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
-  * main -
+  * main - print message to stderr output
   *
   * Return:1
 */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" -
+	Dora Korpar, 2015-10-19\n");
 	return (1);
 }
