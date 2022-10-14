50c8cbda1f22d458067e9053fd81d9d791c4c422
Kat-Devs <katlegophele95@gmail.com>
2022-10-14 16:16:51 -0700

    Source cpde 3 edited

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,15 @@ int main(void)
 {
 	char low_alpha[26] = "abcdefghijklmnopqrstuvwxyz";
 	char up_alpha[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
+	int i, j;
 
 	for (i = 0; i < 26; i++)
 	{
-		putchar(low_alpha[i], up_alpha[i]);
+		putchar(low_alpha[i]);
+	}	
+	for (j = 0; j < 26; j++)
+	{
+		putchar(up_alpha[j]);
 	}
 	putchar('\n');
 	return (0);
