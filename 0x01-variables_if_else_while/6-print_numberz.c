1dd4361787cdb14db6bba75cbe0d00d46b6d185a
Kat-Devs <katlegophele95@gmail.com>
2022-10-14 17:14:24 -0700

    source code 6 added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main -Entry point
+  * Return: Always 0
+  */
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
