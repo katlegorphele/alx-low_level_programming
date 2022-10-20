b5a741b23e3ef17dc4ee9d763248e2e8ed0ed800
Kat-Devs <katlegophele95@gmail.com>
2022-10-20 12:29:32 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * print_line - prints line on the terminal
+  * @n: number of _ to be added to line
+  * Return : always 0
+  */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+		_putchar('n');
+	_putchar('\n');
+}
