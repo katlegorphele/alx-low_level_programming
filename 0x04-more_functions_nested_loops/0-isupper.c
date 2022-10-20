97fd9f43dde0a09976c014daea6e2adf55597e97
Kat-Devs <katlegophele95@gmail.com>
2022-10-20 08:49:59 -0700

    task 0 fix. main.h file updated

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,13 @@
   * Return: 1 if c is uppercase, 0 if c is lowercase
   */
 
-int _isupper(int c) 
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
