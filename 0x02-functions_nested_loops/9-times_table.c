c8621a74c513ca4effdec85ef9262ce6651baa03
Kat-Devs <katlegophele95@gmail.com>
2022-10-19 18:55:00 -0700

    Task 9 added

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - Peints 9x table starting from 0
+ * Return: void
+ */
+void times_table(void)
+{
+	int a, b, res;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			res = a * b;
+			_putchar(48);
+			_putchar(32);
+			if (res <= 9)
+			{
+				_putchar(32);
+				_putchar(res + 48);
+			}
+			else
+			{
+				_putchar((res / 10) + 48);
+				_putchar((res % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
