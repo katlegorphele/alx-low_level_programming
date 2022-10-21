fbbb1db7d247e0bef58aceca9975af7ee2f3069b
Kat-Devs <katlegophele95@gmail.com>
2022-10-21 14:54:55 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  *print_triangle - program that prints a triangle then a new line
+  * @size: determines size of triangel
+  * Return: viod
+  */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size, i++)
+	{
+		for (j = 1; j < size; j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size -1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
