132a30cc39576eee37e9412d65dc933fdf6369d7
Kat-Devs <katlegophele95@gmail.com>
2022-10-24 19:52:07 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string in reverse followed by a new line
+  * @s: input string
+  * Return: void
+  */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[i] == '\0')
+			break;
+		i++;
+	}
+	for (count--; count >=0; count--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
