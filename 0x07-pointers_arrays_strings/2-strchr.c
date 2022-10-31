45804709192eda696bab45475a74bd7cfad88743
Kat-Devs <katlegophele95@gmail.com>
2022-10-31 14:47:34 -0700

    task 2 added

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * _strchr - locates a character in a string
+  * @s: source string
+  * @c: target chararcter
+  * Return: @c else NULL if character not found
+  */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; *(s + 1) != '\0'; i++)
+		if (*(src + 1) == c)
+			return (s + 1);
+}
