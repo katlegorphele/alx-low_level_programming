51aa9515a82e0e38d55d52bcb0da4274038f7a01
Kat-Devs <katlegophele95@gmail.com>
2022-10-31 14:56:48 -0700

    task 2 added

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,8 +10,10 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; *(s + 1) != '\0'; i++)
-		if (*(s + 1) == c)
-			return (s + 1);
-	return('\0');
+	for (i = 0; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return ( s + i)
+	return ('\0');
 }
