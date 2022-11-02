48905bf006f92dbbf0e7e5c24f18e7a084e254df
Kat-Devs <katlegophele95@gmail.com>
2022-11-02 06:48:29 -0700

    fixes for task 2 and 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,7 +2,7 @@
 /**
   * factorial - calculates factorial of a number
   * @n: number to get factorial of
-  * Return : factorial of @n
+  * Return: factorial of @n
   */
 
 int factorial(int n)
@@ -11,6 +11,7 @@ int factorial(int n)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	else return (n * factorial(n - 1));
+	else
+		return (n * factorial(n - 1));
 
 }
