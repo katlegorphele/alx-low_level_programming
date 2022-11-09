c72a048252778086d1db773d813b56d8778b85cc
Kat-Devs <katlegophele95@gmail.com>
2022-11-09 05:04:46 -0800

    Task 1 added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - prints number of arguments passed
+  * @argc: argument counter
+  * @argv: argument vector
+  * Return: 0 if successful
+  */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
