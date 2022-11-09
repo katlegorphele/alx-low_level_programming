4f555e6123c931a9384b27b2c4cef11962ef4fe1
Kat-Devs <katlegophele95@gmail.com>
2022-11-09 05:14:25 -0800

    ignored unused variables

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
   * @argv: arguments vector
   * Return: 0 if succesful
   */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
   * @argv: argument vector
   * Return: 0 if successful
   */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
