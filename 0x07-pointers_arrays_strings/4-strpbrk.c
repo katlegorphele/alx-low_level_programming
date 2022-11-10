84d910c85ea391ab980cabe6f62dd7ae2056e062
Kat-Devs <katlegophele95@gmail.com>
2022-11-10 02:45:44 -0800

    task 4 fix

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,12 +12,12 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; *(s + 1) != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
-				return (s + 1);
+				return (s + i);
 		}
 	}
 	return ('\0');
