73d37b4a541270c6fbdd2ca2a12ab6f78ee423e2
Kat-Devs <katlegophele95@gmail.com>
2022-11-10 12:36:21 -0800

    task 0 library fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
   * create_array - creates array of characters
   * @size: array size
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	ch = malloc(sizeof(c) * size);
 
-	if (cr == NULL)
+	if (ch == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
