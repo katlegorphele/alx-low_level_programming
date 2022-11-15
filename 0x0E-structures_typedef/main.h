5835417052794fa92a28f16c2e78f30230184dc0
Kat-Devs <katlegophele95@gmail.com>
2022-11-15 12:29:37 -0800

    task 0 added

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - structure that stores some
+  * imformation of a dog.
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: dog owner
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
