ad23abce952f47a2ea1f18937bd6c2931837b884
Kat-Devs <katlegophele95@gmail.com>
2022-11-27 08:04:19 -0800

    task 2 added

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+  * array_iterator - exececutes function given as parameter
+  * on each element of array
+  * @array: inout integer array
+  * @size: size of the array
+  @action: pointer to function
+  *
+  * Return: void
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
