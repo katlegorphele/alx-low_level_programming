9d855b4dfba450e16336745916cf05d328232a28
Kat-Devs <katlegophele95@gmail.com>
2022-11-30 12:44:14 -0800

    0x013

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  * print_listint - prints all elements of a list
+  * @h: list head
+  * Return: number of nodes
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t num_nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		num_nudes++;
+	}
+	return (num_nodes);
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  * listint_len - returns number of elements in list
+  * @h: list head
+  * Return: number of nodes
+  */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t num_nodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		num_nodes++;
+	}
+
+	return (num_nodes);
+}
